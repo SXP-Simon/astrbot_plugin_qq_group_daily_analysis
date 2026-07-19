f00067f9bd96fe803f9c83ac9d5bc6042fbfd65c
clown145 <i@kisaki.love>
2026-07-19 18:32:43 +0800

    fix: restore platform adapter exports

--- src/infrastructure/platform/__init__.py
+++ src/infrastructure/platform/__init__.py
@@ -1,4 +1,11 @@
+from .adapters.lark_adapter import LarkAdapter
+from .adapters.onebot_adapter import OneBotAdapter
 from .base import PlatformAdapter
 from .factory import PlatformAdapterFactory
 
-__all__ = ["PlatformAdapterFactory", "PlatformAdapter"]
+__all__ = [
+    "PlatformAdapterFactory",
+    "PlatformAdapter",
+    "OneBotAdapter",
+    "LarkAdapter",
+]
